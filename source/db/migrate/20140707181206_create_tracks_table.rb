c970e9b4dd20e82c8599584b50e3e4faeb45b691
brennontwilliams <brennonw@gmail.com>
2014-07-07 11:22:10 -0700

    Created Tracks Table, keeping each table in its own migration for now

---
+++ source/db/migrate/20140707181206_create_tracks_table.rb
@@ -0,0 +1,10 @@
+class CreateTracksTable < ActiveRecord::Migration
+  def change
+    create_table :tracks do |t|
+      t.string :title
+      t.integer :user_id
+      t.integer :est_completion_time
+      t.boolean :is_completed
+    end
+  end
+end
