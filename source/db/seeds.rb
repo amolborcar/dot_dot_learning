c622d97131e47bc0907dab788ff33185bc37667c
brennontwilliams <brennonw@gmail.com>
2014-07-08 17:51:52 -0700

    Resources displaying correctly, if a little ugly

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -9,9 +9,9 @@
 Track.create(title: "Microeconomics")
 
 Section.create(title: "Law of Demand", track_id: 1)
-Resource.create(title: "Khan Academy Law of Demmand Video", url: "https://www.khanacademy.org/economics-finance-domain/microeconomics/supply-demand-equilibrium", media_type: "video")
-Resource.create(title: "Investopedia Law of Demmand Explanation", url: "http://www.investopedia.com/terms/l/lawofdemand.asp")
-Resource.create(title: "Economist - The Law of Demmand is a Bummer", url: "http://www.economist.com/blogs/democracyinamerica/2013/02/minimum-wage")
+Resource.create(title: "Khan Academy Law of Demmand Video", url: "https://www.khanacademy.org/economics-finance-domain/microeconomics/supply-demand-equilibrium/demand-curve-tutorial/v/law-of-demand", media_type: "video", section_id: 1)
+Resource.create(title: "Investopedia Law of Demmand Explanation", url: "http://www.investopedia.com/terms/l/lawofdemand.asp", section_id: 1)
+Resource.create(title: "Economist - The Law of Demmand is a Bummer", url: "http://www.economist.com/blogs/democracyinamerica/2013/02/minimum-wage", section_id: 1)
 
 Section.create(title: "Demand and prices", track_id: 1)
 
