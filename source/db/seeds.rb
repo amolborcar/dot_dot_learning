de80f0c6125c91f78a995dff8ce74172039b3ade
brennontwilliams <brennonw@gmail.com>
2014-07-10 12:09:08 -0700

    Added icons/glyphs for video and article resource media_type

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -9,9 +9,9 @@
 Track.create(title: "Microeconomics")
 
 Section.create(title: "Law of Demand", track_id: 1)
-Resource.create(title: "Khan Academy Law of Demmand Video", url: "https://www.khanacademy.org/economics-finance-domain/microeconomics/supply-demand-equilibrium/demand-curve-tutorial/v/law-of-demand", media_type: "video", section_id: 1)
-Resource.create(title: "Investopedia Law of Demmand Explanation", url: "http://www.investopedia.com/terms/l/lawofdemand.asp", section_id: 1)
-Resource.create(title: "Economist - The Law of Demmand is a Bummer", url: "http://www.economist.com/blogs/democracyinamerica/2013/02/minimum-wage", section_id: 1)
+Resource.create(title: "Khan Academy Law of Demand Video", url: "https://www.khanacademy.org/economics-finance-domain/microeconomics/supply-demand-equilibrium/demand-curve-tutorial/v/law-of-demand", media_type: "video", section_id: 1)
+Resource.create(title: "Investopedia Law of Demand Explanation", url: "http://www.investopedia.com/terms/l/lawofdemand.asp", section_id: 1)
+Resource.create(title: "Economist - The Law of Demand is a Bummer", url: "http://www.economist.com/blogs/democracyinamerica/2013/02/minimum-wage", section_id: 1)
 
 Section.create(title: "Demand and prices", track_id: 1)
 
