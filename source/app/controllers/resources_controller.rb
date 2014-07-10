0f97339b76de81a961ea569a55d4ec734984194b
brennontwilliams <brennonw@gmail.com>
2014-07-09 20:43:07 -0700

    Correctly returning resource template sans layout from ajax call

--- source/app/controllers/resources_controller.rb
+++ source/app/controllers/resources_controller.rb
@@ -7,7 +7,7 @@ class ResourcesController < ApplicationController
     p "!"*100
     section_id = params[:section_id]
     p current_user.id
-    Resource.create(user_id: current_user.id, section_id: params[:section_id], title: params[:title], url: params[:url])
-    render :add, locals: {}, layout: false
+    resource = Resource.create(user_id: current_user.id, section_id: params[:section_id], title: params[:title], url: params[:url])
+    render :_show_resource, locals: {resource: resource}, layout: false
   end
 end
