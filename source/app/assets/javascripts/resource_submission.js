c7a4fab37e55a38af69a4f3f582924edfc4c0c08
brennontwilliams <brennonw@gmail.com>
2014-07-10 11:45:24 -0700

    Corrected resource adding AJAX to function properly with CSS/HTML resource changes

--- source/app/assets/javascripts/resource_submission.js
+++ source/app/assets/javascripts/resource_submission.js
@@ -17,7 +17,9 @@ $(document).ready(function() {
     // the correct section was suprisingly difficult
     // TODO: refactor to be less hideous; Find something better than hidden spans containing
     // section ids.
-    var resourceSectionId = $(data).find("span").html()
+    //var resourceSectionId = $(data).find("span").html()
+    debugger
+    var resourceSectionId = $(data).find(".resource-section-id").find("span").html()
     var numberOfSections = $(document).find('.section-section-id').length
 
     for(var i=0; i<numberOfSections; i++){
