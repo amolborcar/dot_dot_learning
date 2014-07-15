fe0231bb84243aa48c0eebddd39ee4f44689dd71
brennontwilliams <brennonw@gmail.com>
2014-07-14 17:37:21 -0700

    Updated seed file and did basic styling on the track page

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,37 @@
 
 Track.create(title: "Microeconomics")
 
-Section.create(title: "SECTION Supply and Demand", track_id: 1)
-Subsection.create(title: "Subsection Law of Demand", section_id: 1)
+Section.create(title: "Supply and Demand", track_id: 1)
+
+Subsection.create(title: "Law of Demand", section_id: 1)
+
 Resource.create(title: "Khan Academy Law of Demand", url: "https://www.khanacademy.org/economics-finance-domain/microeconomics/supply-demand-equilibrium/demand-curve-tutorial/v/law-of-demand", media_type: "video", subsection_id: 1)
 Resource.create(title: "Investopedia Law of Demand Explanation", url: "http://www.investopedia.com/terms/l/lawofdemand.asp", subsection_id: 1)
 Resource.create(title: "Economist - The Law of Demand is a Bummer", url: "http://www.economist.com/blogs/democracyinamerica/2013/02/minimum-wage", subsection_id: 1)
-Subsection.create(title: "Subsection Law of Supply", section_id: 1)
+Subsection.create(title: "Law of Supply", section_id: 1)
 
-Section.create(title: "SECTION Demand and prices", track_id: 1)
+Subsection.create(title: "Demand and prices", section_id: 1)
 
-Section.create(title: "SECTION Demand and future prices", track_id: 1)
+Subsection.create(title: "Demand and future prices", section_id: 1)
 
-Section.create(title: "SECTION Demand and Income, Population, and Prefrence Chances", track_id: 1)
+Subsection.create(title: "Demand and Income, Population, and Prefrence Chances", section_id: 1)
 
-Section.create(title: "SECTION Normal and Inferior Goods", track_id: 1)
+Subsection.create(title: "Normal and Inferior Goods", section_id: 1)
+
+
+Section.create(title: "Elasticity", track_id: 1)
+
+Subsection.create(title: "Demand and Elasticity", section_id: 2)
+Resource.create(title: "Khan Academy Price Elasticity of Demand", url: "http://www.khanacademy.org/economics-finance-domain/microeconomics/elasticity-tutorial/price-elasticity-tutorial/v/price-elasticity-of-demand", media_type: "video", subsection_id: 7)
+Resource.create(title: "Price Elasticity of Demand Definition", url: "http://www.investopedia.com/terms/p/priceelasticity.asp", media_type: "article", subsection_id: 7)
+
+Section.create(title: "Consumer and Producer Surplus", track_id: 1)
+
+
+Section.create(title: "Scarcity and Opportunity Cost", track_id: 1)
+
+
+Section.create(title: "Competition", track_id: 1)
+
+
+Section.create(title: "Nash Equilibrium", track_id: 1)
